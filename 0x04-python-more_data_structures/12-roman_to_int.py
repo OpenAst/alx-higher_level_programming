6b30cec7cc1b0ab9d803fbc665bc58ddec1b9b93
OpenAst <isrealme0@gmail.com>
2023-03-15 06:56:06 -0700

    correction to roman_to_int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,20 +1,13 @@
+
 #!/usr/bin/python3
 
-def roman_to_int(roman_string):
-    """
-    Given a Roman numeral string 'roman_strin', returns its integer equivalent.
-    """
-    roman_to_int_dict = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
 
-    prev_val = 0
-    total_val = 0
-    
-    for char in roman_string:
-        curr_val = roman_to_int_dict[char]
-        if curr_val > prev_val:
-            total_val -= prev_val
-        else:
-            total_val += prev_val
-        prev_val = curr_val
-    total_val += prev_val
-    return total_val
+def roman_to_int(roman_string):
+    if not roman_string or type(roman_string) != str:
+        return 0
+    total = 0
+    digits = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    for roman in reversed(roman_string):
+        arabic = digits[roman]
+        total += arabic if total < arabic * 5 else -arabic
+    return total
