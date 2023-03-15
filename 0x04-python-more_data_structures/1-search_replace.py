bfd2b339941aa2f0f69b9a34a5726a254f30f6ff
OpenAst <isrealme0@gmail.com>
2023-03-15 07:08:26 -0700

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
 
-def search_replace(my_List, search, replace):
-    def find_search(element):
-        return element if element != search else replace
-    return list(map(find_search, my_list))
+def search_replace(my_list, search, replace):
+    # Create a new list to hold the replaced elements
+    new_list = []
+
+    # Iterate through the original list
+    for element in original_list:
+        # If the element is the one to replace, add the replacement element to the new list
+        if element == search:
+            new_list.append(replace)
+        # Otherwise, add the original element to the new list
+        else:
+            new_list.append(element)
+
+    # Return the new list with replaced elements
+    return new_list
+
