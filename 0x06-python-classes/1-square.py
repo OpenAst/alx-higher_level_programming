a9c6a61e6f41ca80d7c2c965a0bd1d49ac5cabe1
OpenAst <isrealme0@gmail.com>
2023-03-28 16:01:01 -0700

    square class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# by Israel
+
+class Square:
+    def __init__(self, size)
+    """INitialising the Square class
+    Args: size - represents the size of the square used
+    """
+
+    self.__size = size
