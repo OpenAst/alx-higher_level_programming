68d43a29c333e10752cb0330c3c7140d067892f4
OpenAst <isrealme0@gmail.com>
2023-03-29 15:59:53 -0700

    square

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,36 +1,45 @@
 #!/usr/bin/python3
-""" a module that create a class Square"""
+# 0-square.py by Ehoneah Obed
+"""A module that defines a square """
+
 
 class Square:
-    """a classs that represent a squar
-    Initialising this class
-    Args: size - represent the size of the square
-    Raises:
-    TypeError- if size if not an integer
-    ValueError- if size is less than zero
-    """
-    if not isinstance(size, int):
-        raise TypeError('size must be an integer')
-    if size < 0:
-        raise ValueError('size must be >= 0')
+    """A class that represents a square"""
 
-    self.__size = size
-    
+    def __init__(self, size=0):
+        """Initializing this square class
+        Args:
+            size: represnets the size of the square defined
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than zero
+        """
+
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
     @property
     def size(self):
-        """Retrieves the size of square"""
+        """Retrieves size of square"""
 
         return self.__size
 
     @size.setter
     def size(self, value):
-        """set a new value to the size"""
         if not isinstance(value, int):
             raise TypeError('size must be an integer')
-        if size < 0:
+        if value < 0:
             raise ValueError('size must be >= 0')
         self.__size = value
-    
+
     def area(self):
-        """returns the area of a square"""
-        return(self._size ** 2)
+        """
+        Calculate area of the square
+        Returns: The square of the size
+        """
+
+        return (self.__size ** 2)
