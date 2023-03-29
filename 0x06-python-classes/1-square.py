5985ec0a7d629f945489eb8f54a6c45f10430c42
OpenAst <isrealme0@gmail.com>
2023-03-29 16:14:30 -0700

    private attribute

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-# by Israel
+"""Square class definition"""
+
 
 class Square:
-    def __init__(self, size)
-    """INitialising the Square class
-    Args: size - represents the size of the square used
+    """Represents a square
+    Attributes:
+        __size (int): size of a side of the square
     """
-
-    self.__size = size
+    def __init__(self, size):
+        """Initializes a square
+        Args:
+            size (int): size of a side of the square
+        Returns: None
+        """
+        self.__size = size
