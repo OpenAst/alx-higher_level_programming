9faed8231f28fb9e328bb7eff30e47c8995eefbb
OpenAst <isrealme0@gmail.com>
2023-04-03 16:32:31 -0700

    rectangle defines

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,44 +1,58 @@
 #!/usr/bin/python3
-"""a module that defines a rectangle"""
+"""This program define a class Rectangle with it attributes"""
+
 
 class Rectangle():
-    """class with a private attribute"""
+    """
+    A Rectangle Class with the private instance attributes width and height
+    """
+
     def __init__(self, width=0, height=0):
-        """initiation of class Rectangle
-        Args:
-            width: 0
-            height: 0
         """
-        self.width = width
+        Constructor of the class Rectangle
+          Args:
+            - width (default = 0): int
+            - heigth (default = 0): int
+        """
         self.height = height
+        self.width = width
 
-        @property
-        def width(self):
-            """Getter for attribute width"""
-            return self.__width
+    @property
+    def width(self):
+        """Getter of the property width"""
+        return self.__width
 
-        @width.setter
-        def width(self,value):
-            """Setter for attribute width"""
-            if type(width) is not int:
-                raise TypeError("width must be an integer")
+    @width.setter
+    def width(self, value):
+        """
+        Getter of the property value
+          Args:
+            - value: int
+        """
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
 
-            if value < 0:
-                raise ValueError("width must be >= 0")
-            self.__width = value
+        if value < 0:
+            raise ValueError('width must be >= 0')
 
-        @property
-        def height(self):
-            """Getter for attribute height"""
-            return self.__height
+        self.__width = value
 
-        @height.setter
-        def height(self, value):
-            """Setter for attribute height"""
-            if type(height) is not int:
-                raise TypeError('height must be an integer')
+    @property
+    def height(self):
+        """Getter of the property height"""
+        return self.__height
 
-            if value < 0:
-                raise ValueError('height must be >= 0')
+    @height.setter
+    def height(self, value):
+        """
+        Getter of the property value
+          Args:
+            - value: int
+        """
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
 
-            self.__height = value
+        if value < 0:
+            raise ValueError('height must be >= 0')
+
+        self.__height = value
