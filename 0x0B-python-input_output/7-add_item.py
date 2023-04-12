70a043f5f33265374c20954deceaf0f16185d7d9
OpenAst <isrealme0@gmail.com>
2023-04-12 14:31:11 -0700

    updated

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
-"""
-This program take the file add_item.json, and add the
-parameters to the list inside this file.
-- If the file doesn't exist create it.
-- If no exist parameters do nothing or create the list if the file is empty.
-"""
+"""This module adds all arguments to a Python list and saves them to a file."""
 
-from sys import argv
-from os.path import exists
+import sys
+from typing import List
 
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+def add_items_to_list_and_save(items: List[str], filename: str) -> None:
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-namefile = "add_item.json"
-argc = len(argv)
+    try:
+        current_items = load_from_json_file(filename)
+    except FileNotFoundError:
+        current_items = []
 
-file_list = []
+    updated_items = current_items + items
+    save_to_json_file(updated_items, filename)
 
-if exists(namefile):
-    file_list = load_from_json_file(namefile)
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    add_items_to_list_and_save(args, "add_item.json")
 
-if (argc == 1):
-    save_to_json_file(file_list, namefile)
-else:
-    for index in range(1, argc):
-        file_list.append(argv[index])
-    save_to_json_file(file_list, namefile
