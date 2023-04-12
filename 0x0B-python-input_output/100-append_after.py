99caa7e2f5c461ac7dbee76705f618d2f2c34b02
OpenAst <isrealme0@gmail.com>
2023-04-12 14:38:41 -0700

    append

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,16 +1,29 @@
-
 #!/usr/bin/python3
-"""This module defines a text file insertion function"""
+"""
+This program take a file and read it, search a string
+and add other string on the next line
+"""
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """Inserts text after each line containing a given string in a file
     """
-    text = ""
-    with open(filename) as r:
-        for line in r:
-            text += line
-            if search_string in line:
-                text += new_string
-    with open(filename, "w") as w:
-        w.write(text)
+    Open and reads a file, search a string in all the file.
+    and after each ocurrence add other string (new_string)
+    Args:
+    - filename: string
+    - search_string: string
+    - new_string: string
+    """
+
+    with open(filename, mode="r+", encoding="utf-8") as _file:
+        all_text = _file.readlines()
+        new_text = ""
+
+        for line in all_text:
+            new_text += line
+            if (search_string in line):
+                new_text += new_string
+
+        # This method position the cursor to the beginning of the file
+        _file.seek(0)
+        _file.write(new_text)
