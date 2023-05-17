f1cc07fb099af2e08b61109812423aec4215581e
OpenAst <isrealme0@gmail.com>
2023-05-17 07:16:56 -0700

    update

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,5 @@
----a script that creates a table of id and name
----should not fail if it doesnt exist
-CREATE TABLE IF NOT EXIST first_tablE (id INT, name VARCHAR(256));
+-- Creates a table in the current database
+-- Query to create a table called first_table in the current database in your MySQL server
+CREATE TABLE IF NOT EXISTS first_table (
+id INT,
+name VARCHAR(256));
