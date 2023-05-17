c1188f4c65026a7281741d0f910cfabf1bbcecb0
OpenAst <isrealme0@gmail.com>
2023-05-17 07:12:41 -0700

    task updated

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,4 +1,5 @@
---creates a table called force_name
--- id INT
--- name VARCHAR(256) can’t be null
-CREATE TABLE IF NOT EXIST 'force_name' ('id' INT,'name' VARCHAR(256) NOT NULL);
+-- Creates the table force_name.
+CREATE TABLE IF NOT EXISTS `force_name` (
+    `id`   INT,
+    `name` VARCHAR(256) NOT NULL
+);
