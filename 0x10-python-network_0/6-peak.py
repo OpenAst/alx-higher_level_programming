8574e453bf3d82527c6e55ee153cb5b4e3cf0a67
OpenAst <isrealme0@gmail.com>
2023-06-30 05:14:54 -0700

    updated

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""Algorithms for list of integers."""
-
+"""
+Algorithm for the max of a lsit of integer
+"""
 
 def find_peak(list_of_integers):
     """Finds a peak in a list of unsorted integers."""
     if list_of_integers:
-        list_of_integers.sort(reverse=True)
-        return list_of_integers[0]
+        return max(list_of_integers)
+    return None
