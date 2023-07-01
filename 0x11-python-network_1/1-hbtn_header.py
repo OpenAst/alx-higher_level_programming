9b0fa7c3faa0198e079b765cac3a174bf287ce23
OpenAst <isrealme0@gmail.com>
2023-07-01 14:34:37 -0700

    updated

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """
- displays the value of the X-Request-Id variable found in the header of the response.
- """
+Displays the value of the X-Request-Id variable
+found in the header of the response.
+"""
 
- if __name___ == "__main__":
-     import urllib.request
-     import sys
 
-     url = sys.argv[1]
+if __name__ == "__main__":
+    import sys
+    import urllib.request
 
-     with urllib.request.urlopen(url) as response:
-         print(response.headers["X-Request-Id"])
-
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.headers["X-Request-Id"])
